3ef055fc3fd926fa21cdaeae8cc51bda81d8cee4
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-03-28 19:48:44 -0400

    add method to share data betewn components

--- src/app/form-indicator/form-indicator.component.ts
+++ src/app/form-indicator/form-indicator.component.ts
@@ -1,30 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ChipsStateService } from '../chips-state.service'
 @Component({
   selector: 'app-form-indicator',
   templateUrl: './form-indicator.component.html',
   styleUrls: ['./form-indicator.component.scss']
 })
 export class FormIndicatorComponent implements OnInit {
-
-  constructor() { }
+  chips: Array<Object>;
+  constructor(private chipsState: ChipsStateService) { }
 
   ngOnInit(): void {
+    this.chipsState.actualChipState.subscribe(chipState => this.chips = chipState);
   }
-  chips = [{
-    color: "primary",
-    selected: true,
-    text:"Informacion Personal"
-  },
-  {
-    color: "primary",
-    selected: true,
-    text:"Informacion Familiar"
-  },
-  {
-    color: "primary",
-    selected: true,
-    text:"Registro Academico"
-  }]
+  
 
 }
