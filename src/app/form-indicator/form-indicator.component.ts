c32485293f0ff9622914686ba79aa31be8d60a30
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-03-28 21:59:32 -0400

    add constants file and logic for navigate

--- src/app/form-indicator/form-indicator.component.ts
+++ src/app/form-indicator/form-indicator.component.ts
@@ -1,17 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { ChipsStateService } from '../chips-state.service'
+import { ChipsStateService } from '../chips-state.service';
+import {Router} from '@angular/router';
+import {ChipData} from '../models/CHIPDATA'
+import {completeFormMessage} from '../utilities/constants';
+import {colors,appRoutes} from '../utilities/constants'
 @Component({
   selector: 'app-form-indicator',
   templateUrl: './form-indicator.component.html',
   styleUrls: ['./form-indicator.component.scss']
 })
 export class FormIndicatorComponent implements OnInit {
-  chips: Array<Object>;
-  constructor(private chipsState: ChipsStateService) { }
+
+
+  
+  constructor(private chipsState: ChipsStateService, private router:Router) { }
+  
+  chips: Array<ChipData>;
 
   ngOnInit(): void {
     this.chipsState.actualChipState.subscribe(chipState => this.chips = chipState);
   }
   
+  navigateToOtherForm(clickedChipIndex){
+    let clikedChip = this.chips[clickedChipIndex];
+    if(clikedChip.color == colors.primary){
+      switch(clickedChipIndex){
+        case 0:
+          this.router.navigate([appRoutes.home]);
+          break;
+        case 1:
+          this.router.navigate([appRoutes.familyInfo]);
+          break;
+        case 2:
+          this.router.navigate([appRoutes.academicInfo]);
+      }
+      
+    }else{
+      alert(completeFormMessage);
+    }
+    
+  }
 
 }
