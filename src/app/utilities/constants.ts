d2e6262089eb15b14c2b138ba2f5adc7fb9fe564
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-03-28 21:57:51 -0400

    add file of constants

---
+++ src/app/utilities/constants.ts
@@ -0,0 +1,19 @@
+
+const completeFormMessage:String = "Favor complete el formulario antes de avanzar";
+const appRoutes = {
+    home:"",
+    familyInfo:"familyInfo",
+    academicInfo:"academicInfo"
+};
+
+const colors = {
+    primary:"primary",
+
+}
+export {
+    completeFormMessage,
+    appRoutes,
+    colors
+};
+
+ 
