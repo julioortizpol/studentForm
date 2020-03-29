62281aa4ece209cc236a04a55d7a3a8457c0a6ac
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-03-28 21:58:26 -0400

    add appRoutes constants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import {StudentPersonalFormComponent} from './student-personal-form/student-personal-form.component'
 import {StudentAcademicInfoComponent} from './student-academic-info/student-academic-info.component';
 import {StudentFamiliarInfoComponent} from './student-familiar-info/student-familiar-info.component';
+import {appRoutes} from './utilities/constants'
 
 const routes: Routes = [
   {
-    path:'', 
+    path:appRoutes.home, 
     component:StudentPersonalFormComponent
   },
   {
-    path:"familyInfo",
+    path:appRoutes.familyInfo,
     component:StudentAcademicInfoComponent
   },
   {
-    path:"academicInfo",
+    path:appRoutes.academicInfo,
     component:StudentFamiliarInfoComponent
   }
 ];
