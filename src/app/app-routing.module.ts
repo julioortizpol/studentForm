222571832dfc951758b4e839882a75a646dfbd0a
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-03-28 20:13:59 -0400

    add routes of the parent form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {StudentPersonalFormComponent} from './student-personal-form/student-personal-form.component'
+import {StudentAcademicInfoComponent} from './student-academic-info/student-academic-info.component';
+import {StudentFamiliarInfoComponent} from './student-familiar-info/student-familiar-info.component';
 
 const routes: Routes = [
   {
     path:'', 
     component:StudentPersonalFormComponent
+  },
+  {
+    path:"familyInfo",
+    component:StudentAcademicInfoComponent
+  },
+  {
+    path:"academicInfo",
+    component:StudentFamiliarInfoComponent
   }
 ];
 
